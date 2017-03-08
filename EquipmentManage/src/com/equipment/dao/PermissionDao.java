479f04800694c89e8d866cda23059fd2e8b866c6
vervaingod <742749059@qq.com>
2017-03-08 16:21:28 +0800

    PC

--- EquipmentManage/src/com/equipment/dao/PermissionDao.java
+++
@@ -1,7 +0,0 @@
-package com.equipment.dao;
-
-import com.equipment.bean.PermissionBean;
-
-public interface PermissionDao {
-	public PermissionBean findPermissionByRole(int role) throws Exception;
-}
